dabb2f500b78e17871963b8c1a2f8807f9faf6ba
SuliMEN1 <98652506+SuliMEN1@users.noreply.github.com>
2022-04-10 17:51:14 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLOutput;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat beanCat = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean==bean1);
+        System.out.println(beanCat==beanCat1);
+    }
+}
